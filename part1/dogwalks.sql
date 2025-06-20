cc3ea0346bb16044d31b40ae681b83b6a223f53c
Aaron <1860828@adelaide.edu.au>
2025-06-20 01:47:43 +0000

    [automatic commit] 2025-06-20 01:47:43 UTC 115.70.25.92

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -65,7 +65,8 @@ VALUES
 INSERT INTO Dogs (name, size, owner_id)
 VALUES
 ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
-('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123'))
+('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
+('Rocky', 'large', (SELECT user_id FROM Users WHERE username = 'carol123')),
 
 
 
