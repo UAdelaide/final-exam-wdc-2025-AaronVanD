590f91cfe9bf55a57507fdeb2ac919d6331aa5b9
Aaron <1860828@adelaide.edu.au>
2025-06-20 04:16:31 +0000

    [automatic commit] 2025-06-20 04:16:31 UTC 115.70.25.92

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -42,7 +42,8 @@ router.get('/api/walkrequests/open', async (req, res) => {
         wr.location,
         u.username AS owner_username
       FROM WalkRequests wr
-      JOIN Dogs d ON wr.
+      JOIN Dogs d ON wr.dog_id = d.dog_id
+      JOIN Users u ON d.
       `);
   } catch (err) {
     console.error(err);
