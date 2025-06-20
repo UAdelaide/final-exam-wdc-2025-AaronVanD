00fe3995adca2ed02ab8bee479c2a89ac2fc86f7
Aaron <1860828@adelaide.edu.au>
2025-06-20 01:51:07 +0000

    [automatic commit] 2025-06-20 01:51:07 UTC 115.70.25.92

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -72,8 +72,8 @@ VALUES
 
 INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes,location,status)
 VALUES
-((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 'Parklands', 'Open')
-((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 'Parklands', 'Open')
+((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00',45 , 'Parklands', 'Open')
+((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 08:00:00', 'Parklands', 'Open')
 
 
 
