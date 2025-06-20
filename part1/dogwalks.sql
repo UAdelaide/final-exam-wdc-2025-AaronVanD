34d3d0f43291773618c417059156a631a0523f73
Aaron <1860828@adelaide.edu.au>
2025-06-20 01:44:18 +0000

    [automatic commit] 2025-06-20 01:44:18 UTC 115.70.25.92

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -60,7 +60,7 @@ VALUES
 ('bobwalker','bob@example.com','hashed456','walker'),
 ('carol123','carol@example.com','hashed789','owner'),
 ('dave123','dave@example.com','hashed000','owner'),
-('alice123','alice@example.com','hashed123','owner'),
+('eve123','eve@example.com','hashed001','owner'),
 
 
 
