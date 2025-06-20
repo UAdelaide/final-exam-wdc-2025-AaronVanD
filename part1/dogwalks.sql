09774ebe212e659196feb5f0d457eb72a2affc3d
Aaron <1860828@adelaide.edu.au>
2025-06-20 01:53:09 +0000

    [automatic commit] 2025-06-20 01:53:09 UTC 115.70.25.92

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -75,7 +75,7 @@ VALUES
 ((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30 , 'Parklands', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
 ((SELECT dog_id FROM Dogs WHERE name='Rocky'), '2025-06-11 10:00:00', 60, 'Mountain Trail', 'open'),
-((SELECT dog_id FROM Dogs WHERE name='Rocky'), '2025-06-11 10:00:00', 60, 'Mountain Trail', 'open'),
+((SELECT dog_id FROM Dogs WHERE name='Coco'), '2025-06-11 10:00:00', 60, 'Mountain Trail', 'open'),
 
 
 
