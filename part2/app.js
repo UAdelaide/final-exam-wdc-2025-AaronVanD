18bea247a08bf39eae169c3bbf31f5f2276d1be9
Aaron <1860828@adelaide.edu.au>
2025-06-20 06:00:31 +0000

    [automatic commit] 2025-06-20 06:00:31 UTC 115.70.25.92

--- part2/app.js
+++ part2/app.js
@@ -10,7 +10,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 
-app.use*()
+app.use(session({
+    
+}));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
