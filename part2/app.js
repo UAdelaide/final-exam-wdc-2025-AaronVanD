40fc59ce2ac23f409cf8d65ca19c022a65ea96dd
Aaron <1860828@adelaide.edu.au>
2025-06-20 05:59:51 +0000

    [automatic commit] 2025-06-20 05:59:50 UTC 115.70.25.92

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+c
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 
+app.use*()
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
