752be30277fdfa1c91ceb41d2c02ea2671fe24b5
Aaron <1860828@adelaide.edu.au>
2025-06-20 04:13:22 +0000

    [automatic commit] 2025-06-20 04:13:22 UTC 115.70.25.92

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -35,7 +35,7 @@ router.get('/api/walkrequests/open', async (req, res) => {
   try{
     const [rows] = (await db).execute(`
       SELECT
-        wr.request
+        wr.request_id
       `);
   } catch (err) {
     console.error(err);
