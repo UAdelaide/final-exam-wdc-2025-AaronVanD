77f00eee262e9167a12a955ca52a80e61e896506
Aaron <1860828@adelaide.edu.au>
2025-06-20 01:46:42 +0000

    [automatic commit] 2025-06-20 01:46:42 UTC 115.70.25.92

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -64,7 +64,7 @@ VALUES
 
 INSERT INTO Dogs (name, size, owner_id)
 VALUES
-('Max', 'medium', (SELECT user_id FROM))
+('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
 
 
 
